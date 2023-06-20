836798ddc11534136357d48e3acd3bf802a0d8a3
Diablo-web <kurhadesumedh20@gmail.com>
2023-06-20 08:58:54 +0530

    add ScrollToTop & Main & WrapProvider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Mockman from "mockman-js";
 import './App.css';
+import { ScrollToTop } from "./frontend/components";
+import { Main } from "./frontend/layouts";
 
 function App() {
   return (
     <div className="App">
+      <ScrollToTop>
+        <Main/>
+      </ScrollToTop>
       <h1>Hello World</h1>
       <Mockman />
     </div>
