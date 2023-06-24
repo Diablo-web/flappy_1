e9ddefcd73c20aa2025e040cdfc8deaffbeea802
Diablo-web <kurhadesumedh20@gmail.com>
2023-06-24 15:10:01 +0530

    feat: add mockman

--- src/frontend/layouts/Router/Router.js
+++ src/frontend/layouts/Router/Router.js
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import { SignInPage, SignUpPage } from "../../pages";
 import { NavBar } from "../NavBar";
 import { NavBarMenu } from "../NavBarMenu";
+import Mockman from "mockman-js";
 
 const Router = () => {
   return (
     <Routes>
       <Route element={<NavBar />}>
+      <Route path="/mockman" element={<Mockman/>} />
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
       </Route>
