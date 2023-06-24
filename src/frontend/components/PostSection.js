7a2241f1d0a7a1be5ceb98c28a0be63e734cbad8
Diablo-web <kurhadesumedh20@gmail.com>
2023-06-24 23:09:00 +0530

    add PostSection

---
+++ src/frontend/components/PostSection.js
@@ -0,0 +1,15 @@
+import { Flex } from "@chakra-ui/react";
+import { Post } from "./Post";
+
+const PostSection = ({posts}) => {
+
+  return (
+    <Flex flexGrow={"1"} direction="column" justifyContent={"start"}>
+      {posts.map((post) => (
+        < Post key={post._id} post={post} />
+      ))}
+    </Flex>
+  );
+};
+
+export { PostSection };
