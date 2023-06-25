0485733e82d3ea19201c3d61a85074bb7ba3e72a
Diablo-web <kurhadesumedh20@gmail.com>
2023-06-25 19:07:32 +0530

    add barrel export

--- src/frontend/pages/index.js
+++ src/frontend/pages/index.js
@@ -1,4 +1,6 @@
 export { SignInPage } from "./SignInPage";
 export { SignUpPage } from "./SignUpPage";
 export { BookMarkPage } from "./BookMarkPage";
-export { ExplorePage} from "./ExplorePage"
+export { ExplorePage} from "./ExplorePage";
+export { ProfilePage} from "./ProfilePage";
+export { FeedPage} from "./FeedPage";
