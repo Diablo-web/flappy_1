b717d4059c9999bdb61992836420c74f5b92d842
Diablo-web <kurhadesumedh20@gmail.com>
2023-06-25 19:26:53 +0530

    feat: add toast

--- src/App.js
+++ src/App.js
@@ -2,8 +2,36 @@
 import './App.css';
 import { ScrollToTop } from "./frontend/components";
 import { Router } from "./frontend/layouts";
+import { useToast } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllPosts, getAllUsers } from "./frontend/features";
+
 
 function App() {
+
+  const toast = useToast();
+
+  const { error: postsError } = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (postsError !== "" && postsError.isOnlyPostError) {
+      toast({
+        title: postsError.title,
+        description: postsError.description,
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  }, [postsError, toast]);
+
+  useEffect(() => {
+    dispatch(getAllPosts());
+    dispatch(getAllUsers());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <ScrollToTop>
