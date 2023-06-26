77e49457e12a744005c049141b47a3781f91a8c6
Diablo-web <kurhadesumedh20@gmail.com>
2023-06-26 23:46:43 +0530

    refactor: to dynamic post section

--- src/frontend/components/PostSection.js
+++ src/frontend/components/PostSection.js
@@ -1,19 +1,18 @@
 import { Flex } from "@chakra-ui/react";
 import { Post } from "./Post";
 
-const PostSection = ({posts}) => {
-
+const PostSection = ({ posts }) => {
   return (
-    <Flex direction="column-reverse">
-      {[...Array(31).keys()].slice(1).map((post) => (
-        <Post key={post} post={post} />
-      ))}
-    </Flex>
-    // <Flex flexGrow={"1"} direction="column" justifyContent={"start"}>
-    //   {posts.map((post) => (
-    //     < Post key={post._id} post={post} />
+    // <Flex direction="column-reverse">
+    //   {[...Array(31).keys()].slice(1).map((post) => (
+    //     <Post key={post} post={post} />
     //   ))}
     // </Flex>
+    <Flex flexGrow={"1"} direction="column" justifyContent={"start"}>
+      {posts.map((post) => (
+        <Post key={post._id} post={post} />
+      ))}
+    </Flex>
   );
 };
 
