c7a866305f521ce41844fe5b6d9fd7919cdda489
Diablo-web <kurhadesumedh20@gmail.com>
2023-06-26 23:11:05 +0530

    add reducer ```store.js```

--- src/frontend/store.js
+++ src/frontend/store.js
@@ -1,14 +1,3 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import { authReducer } from "./features";
-
-// const store = configureStore({
-//   reducer: {
-//     auth: authReducer,
-//   },
-// });
-
-// export { store };
-
 import { configureStore } from "@reduxjs/toolkit";
 import {
   authReducer,
