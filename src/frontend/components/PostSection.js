c23fe7f0326c51a31fe06a0b411c964347535784
Diablo-web <kurhadesumedh20@gmail.com>
2023-07-09 18:57:17 +0530

    removed comment

--- src/frontend/components/PostSection.js
+++ src/frontend/components/PostSection.js
@@ -3,11 +3,6 @@ import { Post } from "./Post";
 
 const PostSection = ({ posts }) => {
   return (
-    // <Flex direction="column-reverse">
-    //   {[...Array(31).keys()].slice(1).map((post) => (
-    //     <Post key={post} post={post} />
-    //   ))}
-    // </Flex>
     <Flex flexGrow={"1"} direction="column" justifyContent={"start"}>
       {posts.map((post) => (
         <Post key={post._id} post={post} />
