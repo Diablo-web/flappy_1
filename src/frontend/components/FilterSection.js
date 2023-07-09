7515dd5ec2915a5b2495f0931fc5356e98624c81
Diablo-web <kurhadesumedh20@gmail.com>
2023-07-09 18:55:24 +0530

    refactor:FilterSection

--- src/frontend/components/FilterSection.js
+++ src/frontend/components/FilterSection.js
@@ -1,12 +1,13 @@
-import { Button, Flex, Select } from "@chakra-ui/react";
+import { Button, Flex, Select, useColorModeValue } from "@chakra-ui/react";
 import { MdTrendingUp, MdSyncAlt } from "react-icons/md";
 import { useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { filterShow, filterSortBy } from "../features";
 
-
 const FilterSection = () => {
+  const selectBgColor = useColorModeValue("blue.300", "blue.700");
 
+  const colorModeValue = useColorModeValue(true, false);
   const { pathname } = useLocation();
 
   const { sortBy, show } = useSelector((state) => state.filters);
@@ -14,23 +15,27 @@ const FilterSection = () => {
 
   return (
     <Flex direction={{ base: "row", lg: "column" }} gap={"4"}>
-       {pathname === "/explore" && (
+      {pathname === "/explore" && (
         <>
           <Button
             flexBasis={{ base: "50%", lg: "unset" }}
             leftIcon={<MdTrendingUp />}
-            colorScheme={"purple"}
+            bgColor={selectBgColor ? "blue.300" : "blue.600"}
+            color="white"
             variant={show === "Trending" ? "solid" : "outline"}
             onClick={() => dispatch(filterShow("Trending"))}
+            _hover={{ bgColor: "blue.400" }}
           >
             Show Trending
           </Button>
           <Button
             flexBasis={{ base: "50%", lg: "unset" }}
             leftIcon={<MdSyncAlt />}
-            colorScheme={"purple"}
+            bgColor={colorModeValue ? "gray.100" : "gray.400"}
+            color={selectBgColor}
             variant={show === "Latest" ? "solid" : "outline"}
             onClick={() => dispatch(filterShow("Latest"))}
+            _hover={{ bgColor: "white", borderColor: "gray.400" }}
           >
             Show Latest
           </Button>
@@ -40,9 +45,23 @@ const FilterSection = () => {
         <Select
           value={sortBy}
           onChange={(e) => dispatch(filterSortBy(e.target.value))}
+          bgColor={colorModeValue ? "blue.100" : "gray.600"}
+      
         >
-          <option value={"New to Old"}>New to Old</option>
-          <option value={"Old to New"}>Old to New</option>
+          <option
+            value={"New to Old"}
+        
+            bgColor={colorModeValue ? "blue.100" : "gray.600"}
+          >
+            New to Old
+          </option>
+  
+          <option
+            value={"Old to New"}
+            bgColor={colorModeValue ? "blue.100" : "gray.600"}
+          >
+            Old to New
+          </option>
         </Select>
       )}
     </Flex>
