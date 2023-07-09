85f05a7ce2f635f86b76c7ffa18ecf4b72895e58
Diablo-web <kurhadesumedh20@gmail.com>
2023-07-09 18:49:14 +0530

    refactor: Footer

--- src/frontend/layouts/Footer/Footer.js
+++ src/frontend/layouts/Footer/Footer.js
@@ -1,118 +1,92 @@
 import {
-    Box,
-    Container,
-    Link,
-    SimpleGrid,
-    Stack,
-    Text,
-    // Input,
-    // IconButton,
-    useColorModeValue,
-    Flex,
-    Image,
-    Heading,
-  } from "@chakra-ui/react";
-  import { FaInstagram, FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
+  Box,
+  Container,
+  Link,
+  SimpleGrid,
+  Stack,
+  Text,
+  useColorModeValue,
+  Flex,
+  Image,
+  Heading,
+  IconButton,
+} from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import { FaInstagram, FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
 
-//   import { button } from "../../components";
-  
-  const Footer = () => {
-    return (
-      <Box
-        ml={{ base: 0, md: 60 }}
-        p="4"
-        bg={useColorModeValue("gray.50", "gray.900")}
-        color={useColorModeValue("gray.700", "gray.200")}
-        borderTopWidth="1px"
-        borderTopColor={useColorModeValue("gray.200", "gray.700")}
-      >
-        <Container as={Stack} maxW={"6xl"} py={10}>
-          <SimpleGrid
-            templateColumns={{ base: "1fr 1fr", xl: "2fr 1fr 1fr 1fr" }}
-            spacing={8}
-          >
-            <Stack spacing={6}>
-              <Box>
-                <Flex
-                  as={Link}
-                  to="/"
-                  h="12"
-                  alignItems="center"
-                  gap={2}
-                  _hover={{ textDecoration: "none" }}
-                >
-                  <Image src="/assets/logos/folks-logo.svg" maxH="100%" />
-                  <Heading fontSize="3xl">folks</Heading>
-                </Flex>
-              </Box>
-              <Text fontSize={"sm"}>
-                © 2022 Folks. All rights reserved
-              </Text>
-              <Stack direction={"row"} spacing={6}>
-                <button
-                  label="Instagram"
-                  path="https://www.instagram.com//"
-                >
-                  <FaInstagram />
-                </button>
-                <button
-                  label="Twitter"
-                  path="https://twitter.com/SumedhK01/"
-                >
-                  <FaTwitter />
-                </button>
-                <button label="GitHub" path="https://github.com/Diablo-web">
-                  <FaGithub />
-                </button>
-                <button
-                  label="LinkedIn"
-                  path="https://www.linkedin.com/in/"
-                >
-                  <FaLinkedin />
-                </button>
-              </Stack>
+const Footer = () => {
+  return (
+    <Box
+      ml={{ base: "0", md: "60" }}
+      bg={useColorModeValue("gray.50", "gray.900")}
+      color={useColorModeValue("gray.700", "gray.200")}
+      borderTopWidth="1px"
+      borderTopColor={useColorModeValue("gray.200", "gray.700")}
+    >
+      <Container as={Stack} maxW={"6xl"} py={10}>
+        <SimpleGrid
+          templateColumns={{
+            base: "1fr",
+            sm: "1fr 1fr",
+            xl: "2fr 1fr 1fr 1fr",
+          }}
+          spacing={8}
+          m="4"
+          p="6"
+        >
+          <Stack spacing={10}>
+            <Box>
+              <Flex
+                as={Link}
+                to="/"
+                h="12"
+                alignItems="center"
+                gap={6}
+                _hover={{ textDecoration: "none" }}
+              >
+                <Image src="/assets/logo/flappy.png" maxH="100%" />
+                <Heading fontSize="2xl">flappy</Heading>
+              </Flex>
+            </Box>
+            <Text fontSize="2xl" textAlign="left">
+              © 2022 Flappy. All rights reserved
+            </Text>
+
+            <Stack direction={"row"} fontSize="4xl" spacing={10}>
+              <IconButton
+                as={RouterLink}
+                to="https://www.instagram.com/sumedh__20/"
+                label="Instagram"
+              >
+                <FaInstagram />
+              </IconButton>
+              <IconButton
+                as={RouterLink}
+                to="https://twitter.com/SumedhK01/"
+                label="Twitter"
+              >
+                <FaTwitter />
+              </IconButton>
+              <IconButton
+                as={RouterLink}
+                to="https://github.com/Diablo-web"
+                label="GitHub"
+              >
+                <FaGithub />
+              </IconButton>
+              <IconButton
+                as={RouterLink}
+                to="https://www.linkedin.com/in/sumedh-kurhade-98274a19b/"
+                label="LinkedIn"
+              >
+                <FaLinkedin />
+              </IconButton>
             </Stack>
-            {/* <Stack align={"flex-start"}>
-              <ListHeader>Company</ListHeader>
-              <Link href={"#"}>About us</Link>
-              <Link href={"#"}>Blog</Link>
-              <Link href={"#"}>Contact us</Link>
-              <Link href={"#"}>Pricing</Link>
-              <Link href={"#"}>Testimonials</Link>
-            </Stack>
-            <Stack align={"flex-start"}>
-              <ListHeader>Support</ListHeader>
-              <Link href={"#"}>Help Center</Link>
-              <Link href={"#"}>Terms of Service</Link>
-              <Link href={"#"}>Legal</Link>
-              <Link href={"#"}>Privacy Policy</Link>
-              <Link href={"#"}>Satus</Link>
-            </Stack> */}
-            {/* <Stack align={"flex-start"}>
-              <ListHeader>Stay up to date</ListHeader>
-              <Stack direction={"row"}>
-                <Input
-                  placeholder={"Your email address"}
-                  bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
-                  border={0}
-                  _focus={{
-                    bg: "whiteAlpha.300",
-                  }}
-                />
-                <IconButton
-                  colorScheme="purple"
-                  _hover={{
-                    bg: "green.600",
-                  }}
-                  aria-label="Subscribe"
-                  icon={<BiMailSend />}
-                />
-              </Stack>
-            </Stack> */}
-          </SimpleGrid>
-        </Container>
-      </Box>
-    );
-  };
-  
-  export { Footer };
+          </Stack>
+        </SimpleGrid>
+      </Container>
+    </Box>
+  );
+};
+
+export { Footer };
