e523ce13f001767b19eef525f8c3df470390da35
Diablo-web <kurhadesumedh20@gmail.com>
2023-07-09 13:55:55 +0530

    refactor: Router

--- src/frontend/layouts/Router/Router.js
+++ src/frontend/layouts/Router/Router.js
@@ -1,61 +1,35 @@
 import { Route, Routes } from "react-router-dom";
-// import { SignInPage, SignUpPage } from "../../pages";
 import { NavBar } from "../NavBar";
 import { NavBarMenu } from "../NavBarMenu";
 import Mockman from "mockman-js";
 import { RequireAuth } from "../../components";
 import {
-  BookmarksPage,
   ExplorePage,
+  BookMarkPage,
   FeedPage,
   ProfilePage,
   SignInPage,
   SignUpPage,
+  SinglePostPage,
 } from "../../pages";
 
 const Router = () => {
   return (
     <Routes>
       <Route element={<NavBar />}>
-      <Route path="/mockman" element={<Mockman/>} />
+        <Route path="/mockman" element={<Mockman />} />
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
       </Route>
-      <Route element={<NavBarMenu />}>
 
-      <Route
-          path="/profile"
-          element={
-            <RequireAuth>
-              <ProfilePage />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/"
-          element={
-            <RequireAuth>
-              <FeedPage />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/explore"
-          element={
-            <RequireAuth>
-              <ExplorePage />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/bookmarks"
-          element={
-            <RequireAuth>
-              <BookmarksPage />
-            </RequireAuth>
-          }
-        />
-       
+      <Route element={<RequireAuth />}>
+        <Route element={<NavBarMenu />}>
+          <Route path="/profile/:username" element={<ProfilePage />} />
+          <Route path="/" element={<FeedPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/bookmarks" element={<BookMarkPage />} />
+          <Route path="/post/:postId" element={<SinglePostPage />} />
+        </Route>
       </Route>
     </Routes>
   );
