b5fd836379d662be1bd41021c792d6106562f3ec
Diablo-web <kurhadesumedh20@gmail.com>
2023-07-09 19:15:13 +0530

    refactor: App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-
-import './App.css';
+import "./App.css";
 import { ScrollToTop } from "./frontend/components";
 import { Router } from "./frontend/layouts";
-import { useToast } from "@chakra-ui/react";
+import { Box, useColorModeValue, useToast } from "@chakra-ui/react";
+
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllPosts, getAllUsers } from "./frontend/features";
 
-
 function App() {
-
   const toast = useToast();
 
   const { error: postsError } = useSelector((state) => state.posts);
+  const { error: usersError } = useSelector((state) => state.users);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (postsError !== "" && postsError.isOnlyPostError) {
+    dispatch(getAllPosts());
+    dispatch(getAllUsers());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (postsError !== "") {
       toast({
         title: postsError.title,
         description: postsError.description,
         status: "error",
+        position: "bottom-right",
         duration: 3000,
         isClosable: true,
       });
@@ -28,18 +34,24 @@ function App() {
   }, [postsError, toast]);
 
   useEffect(() => {
-    dispatch(getAllPosts());
-    dispatch(getAllUsers());
-  }, [dispatch]);
+    if (usersError !== "") {
+      toast({
+        title: usersError.title,
+        description: usersError.description,
+        status: "error",
+        position: "bottom-right",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  }, [usersError, toast]);
 
   return (
-    <div className="App">
+    <Box bg={useColorModeValue("white", "gray.900")}>
       <ScrollToTop>
-        <Router/>
+        <Router />
       </ScrollToTop>
-      {/* <h1>Hello World</h1> */}
-    
-    </div>
+    </Box>
   );
 }
 
