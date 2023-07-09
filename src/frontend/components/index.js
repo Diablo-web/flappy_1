2d73673b41bde6bad1b91f375734a54e3236767c
Diablo-web <kurhadesumedh20@gmail.com>
2023-07-09 19:09:52 +0530

    add Barrel Export

---
+++ src/frontend/components/index.js
@@ -0,0 +1,14 @@
+export { ColorModeSwitch } from "./ColorModeSwitch";
+export { CommentsSection } from "./CommentsSection";
+export { CreatePost } from "./CreatePost";
+export { FilterSection } from "./FilterSection";
+export { Header } from "./Header";
+export { Post } from "./Post";
+export { PostSection } from "./PostSection";
+export { ProfileSection } from "./ProfileSection";
+export { RequireAuth } from "./RequireAuth";
+export { Sidebar } from "./SideBar";
+export { NavItem } from "./NavItem";
+export { ScrollToTop } from "./ScrollToTop";
+export { MobileNav } from "./MobileNav";
+export { SuggestedProfiles } from "./SuggestedProfile";
